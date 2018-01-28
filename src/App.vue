c93467261a6a3e39192e4d40abdf9eebbde57f12
temikn <temikng@gmail.com>
2018-01-28 13:14:36 +0300

    create template of vue-double-range component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <h1>Example vue-double-range</h1>
+
+    <vue-double-range
+      :min="initData.slider.min"
+      :max="initData.slider.max"
+      :step="initData.slider.step"
+      v-model="slider"></vue-double-range>
+  </div>
 </template>
 
 <script>
+import VueDoubleRange from './components/VueDoubleRange.vue';
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    VueDoubleRange
+  },
+  data () {
+    const defaultSliderData = {
+      from: 150000,
+      to: 500000
+    };
+    return {
+      initData: {
+        slider: {
+          min: 1000,
+          max: 1000000,
+          step: 1000
+        }
+      },
+      slider: defaultSliderData,
+      dynamicSlider: defaultSliderData
+    }
+  }
 }
 </script>
 
---
+++ src/components/VueDoubleRange.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="container" ref="container">
+    <div class="slider" name="slider" ref="slider">
+      <div class="zoom-char zoom-char--left" name="left"></div>
+      <div class="zoom-char zoom-char--right" name="right"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueDoubleRange',
+  props: {
+    value: Object,
+    min: Number,
+    max: Number,
+    step: Number
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    position: relative;
+    height: 20px;
+    background-color: #cecece;
+    border: 1px solid #9a9a9a;
+    margin: 10px 18px;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  .slider {
+    cursor: -webkit-grab;
+    position: absolute;
+    height: 100%;
+    background-color: #00000080;
+  }
+  .slider--grabbing {
+    cursor: -webkit-grabbing;
+  }
+  .zoom-char {
+    background-image: url('../assets/images/vue-double-range/dragIconRoundBig.svg');
+    cursor: ew-resize;
+    position: absolute;
+    width: 35px;
+    height: 35px;
+    top: -8px;
+  }
+  .zoom-char--left {
+    left: -18px;
+  }
+  .zoom-char--right {
+    right: -18px;
+  }
+</style>
