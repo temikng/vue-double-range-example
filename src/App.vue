19ce1de8aed9ccb3bb751aa3abe2b7f953e27da4
temikn <temikng@gmail.com>
2018-01-28 14:17:59 +0300

    development vue-double-range component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,47 @@
   <div id="app">
     <h1>Example vue-double-range</h1>
 
+    <div>
+      <label>From:
+        <input type="number"
+               v-model="slider.from"
+               :min="initData.slider.min" :max="slider.to"
+               :step="initData.slider.step"
+               @keydown="onKeyDownInputSlider"/>
+      </label>
+      <label>To:
+        <input type="number"
+               v-model="slider.to"
+               :min="slider.from" :max="initData.slider.max"
+               :step="initData.slider.step"
+               @keydown="onKeyDownInputSlider"/>
+      </label>
+    </div>
+
     <vue-double-range
       :min="initData.slider.min"
       :max="initData.slider.max"
       :step="initData.slider.step"
-      v-model="slider"></vue-double-range>
+      v-model="slider"
+      @input="onSliderChange" @update="onSliderUpdate"></vue-double-range>
+
+    <div class="info">
+      On move data:
+      <pre class="info-code">{{dynamicSlider}}</pre>
+      On the end of move data:
+      <pre class="info-code">{{slider}}</pre>
+    </div>
   </div>
 </template>
 
 <script>
 import VueDoubleRange from './components/VueDoubleRange.vue';
+
+const AVAILABLE_KEY_CODES = [9, 35, 36, 37, 38, 39, 40];
+const DEFAULT_SLIDER_DATA = {
+  from: 150000,
+  to: 500000
+};
 
 export default {
   name: 'App',
@@ -19,10 +50,6 @@ export default {
     VueDoubleRange
   },
   data () {
-    const defaultSliderData = {
-      from: 150000,
-      to: 500000
-    };
     return {
       initData: {
         slider: {
@@ -31,8 +58,22 @@ export default {
           step: 1000
         }
       },
-      slider: defaultSliderData,
-      dynamicSlider: defaultSliderData
+      slider: DEFAULT_SLIDER_DATA,
+      dynamicSlider: DEFAULT_SLIDER_DATA
+    }
+  },
+  methods: {
+    onKeyDownInputSlider (event = window.event) {
+      const keyCode = (event.which) ? event.which : event.keyCode;
+      if (AVAILABLE_KEY_CODES.indexOf(keyCode) >= 0) return true;
+      event.preventDefault();
+      return false;
+    },
+    onSliderChange (event) {
+      console.log('onSliderChange:event', event);
+    },
+    onSliderUpdate (event) {
+      this.dynamicSlider = event;
     }
   }
 }
@@ -45,5 +86,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin: 60px 260px 0;
+}
+h1 {
+  text-align: center;
+}
+.info {
+  margin-top: 25px;
+}
+.info-code {
+  background-color: #cecece;
+  border: 1px solid #9a9a9a;
+  padding: 8px;
 }
 </style>
--- src/components/VueDoubleRange.vue
+++ src/components/VueDoubleRange.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="container" ref="container">
-    <div class="slider" name="slider" ref="slider">
-      <div class="zoom-char zoom-char--left" name="left"></div>
-      <div class="zoom-char zoom-char--right" name="right"></div>
+    <div class="slider" name="slider"
+         ref="slider"
+         :style="{ left: inner.left + 'px', width: inner.width + 'px' }"
+         @mousedown="onMouseDown">
+      <div class="zoom-char zoom-char--left" name="left"
+           ref="left"
+           @mousedown="onMouseDown"></div>
+      <div class="zoom-char zoom-char--right" name="right"
+           ref="right"
+           @mousedown="onMouseDown"></div>
     </div>
   </div>
 </template>
@@ -15,6 +22,202 @@ export default {
     min: Number,
     max: Number,
     step: Number
+  },
+  data () {
+    return {
+      isMouseDown: false,
+      movedItem: null,
+      tmpValue: {
+        from: 0,
+        halfFrom: 0,
+        to: 0,
+        halfTo: 0
+      },
+      width: {
+        container: 0,
+        step: 0,
+        halfStep: 0
+      },
+      mouse: {
+        startX: 0
+      }
+    }
+  },
+  mounted () {
+    // check available slider bounded
+    if (this.step <= 0) {
+      this.step = 1;
+    }
+    let value = this.value;
+    if (value.from < this.min) {
+      this.$emit('input', { from: this.min, to: value.to });
+    }
+    if (value.to > this.max) {
+      this.$emit('input', { from: value.from, to: this.max });
+    }
+    if (value.from > value.to) {
+      this.$emit('input', { from: this.min, to: this.max });
+    }
+    // add events
+    window.addEventListener('resize', this.onWindowResize);
+    document.addEventListener('mouseup', this.onMouseUp);
+    document.addEventListener('mousemove', this.onMouseMove);
+    // init width data
+    this.onWindowResize();
+  },
+  beforeDestroy () {
+    // remove added events
+    window.removeEventListener('resize', this.onWindowResize);
+    document.removeEventListener('mouseup', this.onMouseUp);
+    document.removeEventListener('mousemove', this.onMouseMove);
+  },
+  computed: {
+    inner () {
+      let { from, to } = this.getValue();
+      let left = this.width.step * ((from - this.min) / this.step);
+      let width = (this.width.step * ((to - this.min) / this.step)) - left;
+      return {
+        left,
+        width
+      };
+    }
+  },
+  methods: {
+    getValue () {
+      return {
+        from: (Number(this.value.from) + this.tmpValue.from + this.tmpValue.halfFrom) || this.min,
+        to: (Number(this.value.to) + this.tmpValue.to + this.tmpValue.halfTo) || this.max
+      }
+    },
+    onWindowResize () {
+      this.width.container = this.$refs.container.offsetWidth;
+      this.width.step = (this.width.container / (this.max - this.min)) * this.step;
+      this.width.halfStep = this.width.step / 2;
+    },
+    onMouseDown (event) {
+      // check is previously mouse not pressed down
+      if (this.isMouseDown) return;
+      // get info from new mouse down event
+      this.movedItem = event.target.getAttribute('name');
+      this.mouse.startX = event.clientX;
+      switch (this.movedItem) {
+        case 'slider':
+          this.$refs.slider.classList.add('slider--grabbing');
+          break;
+        case 'left':
+          this.$refs.right.style.zIndex = 1;
+          this.$refs.left.style.zIndex = 2;
+          break;
+        case 'right':
+          this.$refs.left.style.zIndex = 1;
+          this.$refs.right.style.zIndex = 2;
+          break;
+      }
+      this.isMouseDown = true;
+    },
+    onMouseMove (event) {
+      // check is previously mouse pressed down
+      if (this.isMouseDown) {
+        let clientX = event.clientX;
+        let delta = this.mouse.startX - clientX;
+        // get new value parts from delta
+        let parts, halfPart;
+        if (delta > 0) {
+          parts = Math.floor(delta / this.width.step);
+          halfPart = Math.floor((delta - (parts * this.width.step)) / this.width.halfStep);
+          parts = -(parts * this.step);
+          halfPart = -(halfPart * this.step);
+        } else {
+          delta = -delta;
+          parts = Math.floor(delta / this.width.step);
+          halfPart = Math.floor((delta - (parts * this.width.step)) / this.width.halfStep);
+          parts = parts * this.step;
+          halfPart = halfPart * this.step;
+        }
+        let valueFrom = Number(this.value.from);
+        let valueTo = Number(this.value.to);
+        switch (this.movedItem) {
+          case 'left': {
+            // check bounds
+            let tmpValueFrom = valueFrom + parts + halfPart;
+            if (tmpValueFrom < this.min) {
+              parts = this.min - valueFrom;
+              halfPart = 0;
+            } else if (tmpValueFrom > valueTo) {
+              parts = valueTo - valueFrom;
+              halfPart = 0;
+            }
+            // set new values
+            this.tmpValue.from = parts;
+            this.tmpValue.halfFrom = halfPart;
+            break;
+          }
+          case 'right': {
+            // check bounds
+            let tmpValueTo = valueTo + parts + halfPart;
+            if (tmpValueTo > this.max) {
+              parts = this.max - valueTo;
+              halfPart = 0;
+            } else if (tmpValueTo < valueFrom) {
+              parts = valueFrom - valueTo;
+              halfPart = 0;
+            }
+            // set new values
+            this.tmpValue.to = parts;
+            this.tmpValue.halfTo = halfPart;
+            break;
+          }
+          case 'slider': {
+            // check bounds
+            let tmpValueFrom = valueFrom + parts + halfPart;
+            let tmpValueTo = valueTo + parts + halfPart;
+            if (tmpValueFrom < this.min) {
+              parts = this.min - valueFrom;
+              halfPart = 0;
+            } else if (tmpValueTo > this.max) {
+              parts = this.max - valueTo;
+              halfPart = 0;
+            }
+            // set new values
+            this.tmpValue.from = parts;
+            this.tmpValue.to = parts;
+            this.tmpValue.halfFrom = halfPart;
+            this.tmpValue.halfTo = halfPart;
+          }
+        }
+        // emit event that moved value update
+        this.$emit('update', this.getValue());
+      }
+    },
+    onMouseUp () {
+      // check is previously mouse pressed down
+      if (!this.isMouseDown) return;
+      // final handling data
+      this.updateValue();
+      if (this.movedItem === 'slider') {
+        this.$refs.slider.classList.remove('slider--grabbing');
+      }
+      this.resetData();
+    },
+    updateValue () {
+      // recalculate new value
+      let prevValueFrom = Number(this.value.from);
+      let prevValueTo = Number(this.value.to);
+      this.value.from = prevValueFrom + this.tmpValue.from + this.tmpValue.halfFrom;
+      this.value.to = prevValueTo + this.tmpValue.to + this.tmpValue.halfTo;
+      if (prevValueFrom !== this.value.from || prevValueTo !== this.value.to) {
+        // emit event to update v-model
+        this.$emit('input', this.value);
+      }
+    },
+    resetData () {
+      this.tmpValue.from = 0;
+      this.tmpValue.halfFrom = 0;
+      this.tmpValue.to = 0;
+      this.tmpValue.halfTo = 0;
+      this.movedItem = null;
+      this.isMouseDown = false;
+    }
   }
 }
 </script>
